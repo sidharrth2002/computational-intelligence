7cde24dde9f171c93bbb0981ad99d8a60d9fab03
Sidharrth Nagappan <sidharrth2002@gmail.com>
2022-10-15 17:25:31 +0200

    terrible?

--- lab1/solution v2.py
+++ lab1/solution v2.py
@@ -16,6 +16,7 @@ class Graph:
         self.final_list = []
         # N is the count of elements that should be in the final list
         self.N = N
+        self.discovered_elements = set()
 
     def flatten_list(self, _list):
         return list(itertools.chain.from_iterable(_list))
@@ -25,16 +26,16 @@ class Graph:
 
     def get_number_of_elements_not_in_second_list(self, list1, list2):
         count = 0
-        flattened_list = self.flatten_list(list2)
+        # flattened_list = self.flatten_list(list2)
         for i in list1:
-            if i not in flattened_list:
+            if i not in list2:
                 count += 1
         return count
 
     # f(n) = h(n) + g(n)
 
     def h(self, n):
-        num_new_elements = self.get_number_of_elements_not_in_second_list(self.list_values[n], self.final_list)
+        num_new_elements = self.get_number_of_elements_not_in_second_list(self.list_values[n], self.discovered_elements)
         if self.list_values[n] in self.final_list:
             return 100
         return self.H[n] - num_new_elements
@@ -52,9 +53,9 @@ class Graph:
     # visited_node = [1, 2, 3]
     # final_list = [[4, 5], [1]]
     def are_we_done(self):
-        flattened_list = list(itertools.chain.from_iterable(self.final_list))
+        # flattened_list = list(itertools.chain.from_iterable(self.final_list))
         for i in range(self.N):
-            if i not in flattened_list:
+            if i not in self.discovered_elements:
                 return False
         print("We are done")
         return True
@@ -101,6 +102,11 @@ class Graph:
 
             print(f"Visiting node: {n}")
             self.final_list.append(self.list_values[n])
+            # self.discovered_elements.union(self.list_values[n])
+            # add list_values[n] to discovered_elements
+            for i in self.list_values[n]:
+                self.discovered_elements.add(i)
+            print(len(self.discovered_elements))
 
             # if the current node is the stop_node
             # then we begin reconstructin the path from it to the start_node
@@ -108,7 +114,6 @@ class Graph:
                 reconst_path = []
 
                 while parents[n] != n:
-                    print(parents[n])
                     reconst_path.append(n)
                     n = parents[n]
 
@@ -116,10 +121,8 @@ class Graph:
 
                 reconst_path.reverse()
 
-                print(self.final_list)
                 print(f"Number of elements in final list: {len(self.flatten_list(self.final_list))}")
                 print('Path found: {}'.format(reconst_path))
-                print(f"Parents: {parents}")
                 print(
                     f"I guess better looking solution for N={N}: w={sum(len(_) for _ in self.final_list)} (bloat={(sum(len(_) for _ in self.final_list)-N)/N*100:.0f}%)"
                 )
@@ -145,7 +148,6 @@ class Graph:
                     if g[m] + self.h(m) > g[n] + self.h(n) + weight:
                         g[m] = g[n] + weight
                         parents[m] = n
-                        print(f"Updating parent of {m} to {n}")
 
                         if m in closed_list:
                             closed_list.remove(m)
@@ -172,8 +174,9 @@ def problem(N, seed=None):
     ]
 
 # list_of_lists = [[3], [0, 6], [1, 7], [3], [1], [5, 6], [6], [3], [1, 4, 6], [7], [2, 4], [1, 2, 3], [0, 1, 6], [2, 7], [5, 7], [0], [7], [4, 7], [2], [3, 6], [4, 5, 6], [4, 5, 7], [2, 4], [0, 1, 3], [0, 4, 6], [7]]
-N = 200
+N = 1000
 list_of_lists = problem(N, 42)
+print(max(list(itertools.chain.from_iterable(list_of_lists))))
 
 list_values = {}
 for i in range(len(list_of_lists)):
@@ -226,7 +229,6 @@ def greedy(N):
         if not set(x) < covered:
             solution.append(x)
             covered |= set(x)
-    print(solution)
     print(f"Number of elements in final list: {len(flatten_list(solution))}")
     print(
         f"Greedy solution for N={N}: w={sum(len(_) for _ in solution)} (bloat={(sum(len(_) for _ in solution)-N)/N*100:.0f}%)"
