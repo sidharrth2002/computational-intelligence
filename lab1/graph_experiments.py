8cb5190577403bc140ce81d6b53edff3299ceb76
Sidharrth Nagappan <sidharrth2002@gmail.com>
2022-10-16 20:10:09 +0200

    update notebook

--- lab1/graph_experiments.py
+++ lab1/graph_experiments.py
@@ -1,5 +1,3 @@
-# sid
-
 from collections import deque
 import itertools
 import random
@@ -18,6 +16,7 @@ class Graph:
         self.final_list = []
         # N is the count of elements that should be in the final list
         self.N = N
+        self.discovered_elements = set()
 
     def flatten_list(self, _list):
         return list(itertools.chain.from_iterable(_list))
@@ -27,19 +26,22 @@ class Graph:
 
     def get_number_of_elements_not_in_second_list(self, list1, list2):
         count = 0
-        flattened_list = self.flatten_list(list2)
-        for i in list1:
-            if i not in flattened_list:
+        # flattened_list = self.flatten_list(list2)
+        for i in set(list1):
+            # print("i: ", i)
+            if i not in list2:
                 count += 1
-        return count
+        # if count > 1:
+        #     print("count: ", count)
+        return len(set(list1) - set(list2))
 
     # f(n) = h(n) + g(n)
 
     def h(self, n):
-        num_new_elements = self.get_number_of_elements_not_in_second_list(self.list_values[n], self.final_list)
-        if self.list_values[n] in self.final_list:
-            return 100
-        return self.H[n] - num_new_elements
+        num_new_elements = self.get_number_of_elements_not_in_second_list(self.list_values[n], self.discovered_elements)
+        # if self.list_values[n] in self.final_list:
+        #     return 1000
+        return num_new_elements
         # return self.H[n] / (num_new_elements + 1)
 
     def get_node_with_least_h(self):
@@ -51,12 +53,21 @@ class Graph:
                 min_node = node
         return min_node
 
+    def get_node_with_least_h_and_not_in_final_list(self):
+        min_h = float("inf")
+        min_node = None
+        for node in self.adjacency_list:
+            if self.h(node) < min_h and node not in self.final_list:
+                min_h = self.h(node)
+                min_node = node
+        return min_node
+
     # visited_node = [1, 2, 3]
     # final_list = [[4, 5], [1]]
     def are_we_done(self):
-        flattened_list = list(itertools.chain.from_iterable(self.final_list))
+        # flattened_list = list(itertools.chain.from_iterable(self.final_list))
         for i in range(self.N):
-            if i not in flattened_list:
+            if i not in self.discovered_elements:
                 return False
         print("We are done")
         return True
@@ -92,9 +103,9 @@ class Graph:
         while len(open_list) > 0:
             n = None
 
-            # find a node with the lowest value of f() - evaluation function
+            # find a node with the highest value of f() - evaluation function
             for v in open_list:
-                if n == None or g[v] + self.h(v) < g[n] + self.h(n):
+                if n == None or g[v] + self.h(v) > g[n] + self.h(n):
                     n = v;
 
             if n == None:
@@ -103,6 +114,11 @@ class Graph:
 
             print(f"Visiting node: {n}")
             self.final_list.append(self.list_values[n])
+            # self.discovered_elements.union(self.list_values[n])
+            # add list_values[n] to discovered_elements
+            for i in self.list_values[n]:
+                self.discovered_elements.add(i)
+            print(len(self.discovered_elements))
 
             # if the current node is the stop_node
             # then we begin reconstructin the path from it to the start_node
@@ -110,7 +126,6 @@ class Graph:
                 reconst_path = []
 
                 while parents[n] != n:
-                    print(parents[n])
                     reconst_path.append(n)
                     n = parents[n]
 
@@ -118,11 +133,10 @@ class Graph:
 
                 reconst_path.reverse()
 
-                print(self.final_list)
                 print(f"Number of elements in final list: {len(self.flatten_list(self.final_list))}")
                 print('Path found: {}'.format(reconst_path))
                 print(
-                    f"I guess better looking solution for N={N}: w={sum(len(_) for _ in self.final_list)} (bloat={(sum(len(_) for _ in self.final_list)-N)/N*100:.0f}%)"
+                    f"My solution for N={N}: w={sum(len(_) for _ in self.final_list)} (bloat={(sum(len(_) for _ in self.final_list)-N)/N*100:.0f}%)"
                 )
                 return reconst_path
 
@@ -134,6 +148,8 @@ class Graph:
                 if m not in open_list and m not in closed_list:
                     # open_list.add(m)
                     open_list = self.insert_unique_element_into_list(open_list, m)
+                    # sort open_list by self.h
+                    open_list = sorted(open_list, key=self.h)
                     parents[m] = n
                     g[m] = g[n] + weight
 
@@ -144,16 +160,18 @@ class Graph:
                     if g[m] + self.h(m) > g[n] + self.h(n) + weight:
                         g[m] = g[n] + weight
                         parents[m] = n
-                        print(f"Updating parent of {m} to {n}")
 
-                        if m in closed_list:
-                            closed_list.remove(m)
-                            # open_list.add(m)
-                            open_list = self.insert_unique_element_into_list(open_list, m)
+                        # if m in closed_list:
+                        #     closed_list.remove(m)
+                        #     # open_list.add(m)
+                        #     open_list = self.insert_unique_element_into_list(open_list, m)
+                        #     open_list = sorted(open_list, key=self.h)
+
 
             # remove n from the open_list, and add it to closed_list
             # because all of his neighbors were inspected
             open_list.remove(n)
+            open_list = sorted(open_list, key=self.h)
             # closed_list.add(n)
             closed_list = self.insert_unique_element_into_list(closed_list, n)
 
@@ -168,19 +186,20 @@ def problem(N, seed=None):
     ]
 
 # list_of_lists = [[3], [0, 6], [1, 7], [3], [1], [5, 6], [6], [3], [1, 4, 6], [7], [2, 4], [1, 2, 3], [0, 1, 6], [2, 7], [5, 7], [0], [7], [4, 7], [2], [3, 6], [4, 5, 6], [4, 5, 7], [2, 4], [0, 1, 3], [0, 4, 6], [7]]
-N = 20
-list_of_lists = problem(N, 42)
+N = 1000
+# list_of_lists = problem(N, 42)
+# print(max(list(itertools.chain.from_iterable(list_of_lists))))
 
-list_values = {}
-for i in range(len(list_of_lists)):
-    list_values[str(i)] = list_of_lists[i]
+# list_values = {}
+# for i in range(len(list_of_lists)):
+#     list_values[str(i)] = list_of_lists[i]
 
-adjacency_list = {}
-for i in range(len(list_of_lists)):
-    adjacency_list[str(i)] = []
-    for j in range(len(list_of_lists)):
-        if i != j:
-            adjacency_list[str(i)].append((str(j), 1))
+# adjacency_list = {}
+# for i in range(len(list_of_lists)):
+#     adjacency_list[str(i)] = []
+#     for j in range(len(list_of_lists)):
+#         if i != j:
+#             adjacency_list[str(i)].append((str(j), 1))
 
 # list_values = {
 #     'A': [0, 1],
@@ -195,12 +214,24 @@ for i in range(len(list_of_lists)):
 #     'C': [('A', len(list_values['A'])), ('B', len(list_values['B'])), ('D', len(list_values['D']))],
 #     'D': [('A', len(list_values['A'])), ('B', len(list_values['B'])), ('C', len(list_values['C']))]
 # }
-graph1 = Graph(adjacency_list, list_values, N=N)
+for N in [20]:
+    list_of_lists = problem(N, 42)
+    list_values = {}
+    for i in range(len(list_of_lists)):
+        list_values[str(i)] = list_of_lists[i]
 
-start_time = time.time()
-graph1.a_star_algorithm()
-end_time = time.time()
-print(f"Time taken: {end_time - start_time}")
+    adjacency_list = {}
+    for i in range(len(list_of_lists)):
+        adjacency_list[str(i)] = []
+        for j in range(len(list_of_lists)):
+            if i != j:
+                adjacency_list[str(i)].append((str(j), len(list_values[str(j)])))
+
+    graph1 = Graph(adjacency_list, list_values, N=N)
+    start_time = time.time()
+    graph1.a_star_algorithm()
+    end_time = time.time()
+    print(f"Time taken: {end_time - start_time}")
 
 # def problem(N, seed=None):
 #     random.seed(seed)
@@ -222,7 +253,6 @@ def greedy(N):
         if not set(x) < covered:
             solution.append(x)
             covered |= set(x)
-    print(solution)
     print(f"Number of elements in final list: {len(flatten_list(solution))}")
     print(
         f"Greedy solution for N={N}: w={sum(len(_) for _ in solution)} (bloat={(sum(len(_) for _ in solution)-N)/N*100:.0f}%)"
---
+++ lab1/helpers.py
@@ -0,0 +1,58 @@
+# Copyright © 2022 Giovanni Squillero <squillero@polito.it>
+# https://github.com/squillero/computational-intelligence
+# Free for personal or classroom use; see 'LICENSE.md' for details.
+
+import heapq
+import numpy as np
+
+class State:
+    def __init__(self, data: np.ndarray):
+        self._data = data.copy()
+        self._data.flags.writeable = False
+
+    def __hash__(self):
+        return hash(bytes(self._data))
+
+    def __eq__(self, other):
+        return bytes(self._data) == bytes(other._data)
+
+    def __lt__(self, other):
+        return bytes(self._data) < bytes(other._data)
+
+    def __str__(self):
+        return str(self._data)
+
+    def __repr__(self):
+        return repr(self._data)
+
+    @property
+    def data(self):
+        return self._data
+
+    def copy_data(self):
+        return self._data.copy()
+
+class PriorityQueue:
+    """A basic Priority Queue with simple performance optimizations"""
+
+    def __init__(self):
+        self._data_heap = list()
+        self._data_set = set()
+
+    def __bool__(self):
+        return bool(self._data_set)
+
+    def __contains__(self, item):
+        return item in self._data_set
+
+    def push(self, item, p=None):
+        assert item not in self, f"Duplicated element"
+        if p is None:
+            p = len(self._data_set)
+        self._data_set.add(item)
+        heapq.heappush(self._data_heap, (p, item))
+
+    def pop(self):
+        p, item = heapq.heappop(self._data_heap)
+        self._data_set.remove(item)
+        return item
