fded8381aef52132b9094f86db63739867d6d314
Sidharrth Nagappan <sidharrth2002@gmail.com>
2022-12-06 19:52:42 +0100

    add pruning

--- lab3/minmax.py
+++ lab3/minmax.py
@@ -19,13 +19,13 @@ class MinMaxAgent:
         *_, result = accumulate(nim.rows, xor)
         return result
 
-    def minmax(self, nim: Nim, depth: int, maximizing_player: bool):
+    def minmax(self, nim: Nim, depth: int, maximizing_player: bool, alpha: int = -1, beta: int = 1):
         '''
-        Minimax algorithm to find the best move
+        Minimax algorithm to find the best move with alpha-beta pruning
         '''
         if depth == 0 or nim.goal():
             return self.nim_sum(nim)
-        
+
         if maximizing_player:
             value = -math.inf
             for r, c in enumerate(nim.rows):
@@ -33,7 +33,10 @@ class MinMaxAgent:
                     # make copy of nim object before running a nimming operation
                     replicated_nim = deepcopy(nim)
                     replicated_nim.nimming_remove(r, o)
-                    value = max(value, self.minmax(replicated_nim, depth-1, False))
+                    value = max(value, self.minmax(replicated_nim, depth-1, False, alpha, beta))
+                    alpha = max(alpha, value)
+                    if beta <= alpha:
+                        break
             return value
         else:
             value = math.inf
@@ -42,8 +45,33 @@ class MinMaxAgent:
                     # make copy of nim object before running a nimming operation
                     replicated_nim = deepcopy(nim)
                     replicated_nim.nimming_remove(r, o)
-                    value = min(value, self.minmax(replicated_nim, depth-1, True))
+                    value = min(value, self.minmax(replicated_nim, depth-1, True, alpha, beta))
+                    beta = min(beta, value)
+                    if beta <= alpha:
+                        break
             return value
+
+        # if depth == 0 or nim.goal():
+        #     return self.nim_sum(nim)
+        
+        # if maximizing_player:
+        #     value = -math.inf
+        #     for r, c in enumerate(nim.rows):
+        #         for o in range(1, c+1):
+        #             # make copy of nim object before running a nimming operation
+        #             replicated_nim = deepcopy(nim)
+        #             replicated_nim.nimming_remove(r, o)
+        #             value = max(value, self.minmax(replicated_nim, depth-1, False))
+        #     return value
+        # else:
+        #     value = math.inf
+        #     for r, c in enumerate(nim.rows):
+        #         for o in range(1, c+1):
+        #             # make copy of nim object before running a nimming operation
+        #             replicated_nim = deepcopy(nim)
+        #             replicated_nim.nimming_remove(r, o)
+        #             value = min(value, self.minmax(replicated_nim, depth-1, True))
+        #     return value
 
     def play(self, nim: Nim):
         '''
@@ -61,6 +89,8 @@ class MinMaxAgent:
                     best_move = (r, o)
         self.num_moves += 1
         return best_move
+
+rounds = 10
 
 nim = Nim(num_rows=5)
 agent = MinMaxAgent()
