16ce1420a6273942b1be0e790b73552b97b1e69e
Sidharrth Nagappan <sidharrth2002@gmail.com>
2022-12-09 00:12:16 +0100

    update rl agent

--- lab3/reinforcement_agent.py
+++ lab3/reinforcement_agent.py
@@ -1,6 +1,7 @@
 
 from copy import deepcopy
 import math
+import os
 import random
 from evolved_nim import BrilliantEvolvedAgent
 from lib import Nim
@@ -14,7 +15,7 @@ def unhash_list(l):
 class NimReinforcementAgent:
     """An agent that learns to play Nim through reinforcement learning."""
 
-    def __init__(self, num_rows: int, epsilon: float = 0.1, alpha: float = 0.5, gamma: float = 0.9):
+    def __init__(self, num_rows: int, epsilon: float = 0.8, alpha: float = 0.4, gamma: float = 0.7):
         """Initialize agent."""
         self.num_rows = num_rows
         self.epsilon = epsilon
@@ -29,7 +30,7 @@ class NimReinforcementAgent:
         '''Initialize reward for state with random uniform values'''
         for r, c in enumerate(state.rows):
             for o in range(1, c+1):
-                self.set_Q(hash_list(state.rows), (r, o), random.uniform(1, 0.1))
+                self.set_Q(hash_list(state.rows), (r, o), random.uniform(0, 1))
 
     def get_Q(self, state: Nim, action: tuple):
         """Return Q-value for state and action."""
@@ -45,9 +46,12 @@ class NimReinforcementAgent:
     def get_max_Q(self, state: Nim):
         """Return maximum Q-value for state."""
         max_Q = -math.inf
+        # print(state.rows)
         for r, c in enumerate(state.rows):
             for o in range(1, c+1):
+                # print("Just Q: {}".format(self.get_Q(state, (r, o))))
                 max_Q = max(max_Q, self.get_Q(state, (r, o)))
+        # print("Max Q: {}".format(max_Q))
         return max_Q
 
     def get_possible_actions(self, state: Nim):
@@ -74,9 +78,32 @@ class NimReinforcementAgent:
             return best_action
 
 
-    def update_Q(self, reward: int):
+    def update_Q(self, reward: int, game_over: bool):
         """Update Q-value for previous state and action."""
-        self.set_Q(hash_list(self.previous_state.rows), self.previous_action, self.get_Q(self.previous_state, self.previous_action) + self.alpha * (reward + self.gamma * self.get_max_Q(self.current_state) - self.get_Q(self.previous_state, self.previous_action)))
+        # print("Previous state: {}".format(self.previous_state.rows))
+        # print("Previous action: {}".format(self.previous_action))
+        # new_value = hash_list(self.previous_state.rows), self.previous_action, self.get_Q(self.previous_state, self.previous_action) + self.alpha * (reward + self.gamma * self.get_max_Q(self.current_state) - self.get_Q(self.previous_state, self.previous_action))
+        # Q(s, a) <- old value estimate + alpha * (new value estimate - old value estimate)
+
+        # If the game was over, new_state is not a valid state.
+        if game_over:
+            self.set_Q(hash_list(self.previous_state.rows), self.previous_action, reward)
+        else:
+            # self.q[state][action] = self.q[state][action] + learning_rate * (reward + discount_factor * max(self.q[new_state]) - self.q[state][action])
+            self.set_Q(hash_list(self.previous_state.rows), self.previous_action, self.get_Q(self.previous_state, self.previous_action) + self.alpha * (reward + self.gamma * self.get_max_Q(self.current_state) - self.get_Q(self.previous_state, self.previous_action)))
+
+            # old_value_estimate = self.get_Q(self.previous_state, self.previous_action)
+            # new_value_estimate = reward + self.gamma * self.get_max_Q(self.current_state)
+            # new_q = old_value_estimate + self.alpha * (new_value_estimate - old_value_estimate)
+            # new_value = hash_list(self.previous_state.rows), self.previous_action, new_q
+            # self.set_Q(*new_value)
+
+        # old_value_estimate = self.get_Q(self.previous_state, self.previous_action)
+        # new_value_estimate = reward + self.gamma * self.get_max_Q(self.current_state)
+        # new_q = old_value_estimate + self.alpha * (new_value_estimate - old_value_estimate)
+        # new_value = hash_list(self.previous_state.rows), self.previous_action, new_q
+        # # print("New value: {}".format(new_value))
+        # self.set_Q(*new_value)
 
     def calculate_reward(self, state: Nim):
         """Return reward for state."""
@@ -85,36 +112,54 @@ class NimReinforcementAgent:
         else:
             return -1
 
-    def learn(self, state: Nim, reward: int, random_agent):
+    def learn_play_random(self, random_agent):
         """Train agent by playing against a random agent."""
         agent_wins = 0
-        for i in range(20000):
-            self.current_state = deepcopy(state)
+        for episode in range(100000):
+            nim = Nim(num_rows=3)
+            self.current_state = nim
             self.previous_state = None
             self.previous_action = None
             player = 0
-            while not self.current_state.goal():
+            while True:
+                reward = 0
                 if player == 0:
                     self.previous_state = deepcopy(self.current_state)
+                    # print("Current state: {}".format(self.current_state.rows))
                     self.previous_action = self.get_action(self.current_state)
                     if self.previous_action is None:
                         # make random move if no possible actions
                         print("Making random move")
                         self.current_state.nimming_remove(*random.choice(self.get_possible_actions(self.current_state)))
                     else:
+                        print("RL action: {}".format(self.previous_action))
                         self.current_state.nimming_remove(*self.previous_action)
-                    reward = self.calculate_reward(self.current_state)
-                    self.update_Q(reward)
                     player = 1
                 else:
                     move = random_agent.random_agent(self.current_state)
+                    print("Random agent move: {}".format(move))
                     self.current_state.nimming_remove(*move)
                     player = 0
-            winner = 1 - player
-            if winner == 0:
-                agent_wins += 1
-            self.update_Q(reward)
-        print("Agent won {} out of {} games".format(agent_wins, 10000))
+
+                # learning by calculating reward for the current state
+
+                if self.current_state.goal():
+                    winner = 1 - player
+                    if winner == 0:
+                        agent_wins += 1
+                        reward = 1
+                    else:
+                        reward = -1
+                    self.update_Q(reward, self.current_state.goal())
+                    break
+                else:
+                    self.update_Q(reward, self.current_state.goal())
+
+        # decay epsilon after each episode
+        self.epsilon = self.epsilon - 0.09
+
+        print("Agent won {} out of {} games".format(agent_wins, 100000))
+        print(self.Q)
 
     def choose_action(self, state: Nim):
         """Return action based on greedy policy."""
@@ -132,34 +177,39 @@ class NimReinforcementAgent:
             # make random move if no possible actions
             return random.choice(self.get_possible_actions(state))
         else:
+            # print(f"Best action in {state.rows}: Removed {best_action[1]} objects from row {best_action[0]}")
             return best_action
 
-rounds = 10
+rounds = 20
 minmax_wins = 0
-for i in range(rounds):
-    nim = Nim(num_rows=3)
-    agent = NimReinforcementAgent(num_rows=3)
-    random_agent = BrilliantEvolvedAgent()
-    agent.init_reward(nim)
-    agent.learn(nim, 0, random_agent)
-    player = 0
-    while not nim.goal():
-        if player == 0:
-            move = agent.choose_action(nim)
-            print(f"Reinforcement move: Removed {move[1]} objects from row {move[0]}")
-            print(nim.rows)
-            nim.nimming_remove(*move)
-        else:
-            move = random_agent.random_agent(nim)
-            print(f"Random move {random_agent.num_moves}: Removed {move[1]} objects from row {move[0]}")
-            print(nim.rows)
-            nim.nimming_remove(*move)
-        player = 1 - player
 
-    winner = 1 - player
-    if winner == 0:
-        minmax_wins += 1
-    # player that made the last move wins
-    print(f"Player {winner} wins in round {i+1}!")
+nim = Nim(num_rows=5)
+print(nim.rows)
+agent = NimReinforcementAgent(num_rows=5)
+random_agent = BrilliantEvolvedAgent()
+agent.init_reward(nim)
+agent.learn_play_random(random_agent)
 
-print(f"Reinforcement agent wins {minmax_wins} out of {rounds} rounds")
+# for i in range(rounds):
+#     nim = Nim(num_rows=5)
+#     player = 1
+#     while not nim.goal():
+#         if player == 0:
+#             move = agent.choose_action(nim)
+#             print(f"Reinforcement move: Removed {move[1]} objects from row {move[0]}")
+#             nim.nimming_remove(*move)
+#             print(nim.rows)
+#         else:
+#             move = random_agent.random_agent(nim)
+#             print(f"Random move {random_agent.num_moves}: Removed {move[1]} objects from row {move[0]}")
+#             nim.nimming_remove(*move)
+#             print(nim.rows)
+#         player = 1 - player
+
+#     winner = 1 - player
+#     if winner == 0:
+#         minmax_wins += 1
+#     # player that made the last move wins
+#     print(f"Player {winner} wins in round {i+1}!")
+
+# print(f"Reinforcement agent wins {minmax_wins} out of {rounds} rounds")
